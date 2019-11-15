14aa16b72b924f907182ae0568b27ea773ab7487
Ryan McLaughlin <rlmclaughlin@yahoo.com>
2019-11-14 20:32:42 -0600

    added put request for rooms table

--- router/roomsRouter.js
+++ router/roomsRouter.js
@@ -20,4 +20,16 @@ router.post('/', (req, res) => {
     })
 })
 
+router.put('/:id', (req, res) => {
+    const body = req.body
+    const {id} = req.params
+    db('rooms').where({id}).update(body)
+      .then(response => {
+          res.status(200).json(response)
+      })
+      .catch(error => {
+          res.status(500).json({Message: "There was an error updating the information", error})
+      })
+})
+
 module.exports = router 
